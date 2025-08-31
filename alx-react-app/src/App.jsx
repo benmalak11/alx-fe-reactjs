8331f42f765bfddcc200a0983ef96ba2e29f4677
benalak11 <mm8584856@gmail..com>
2025-08-31 02:54:49 +0100

    task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import UserProfile from './components/UserProfile';
+
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -46,6 +48,18 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </div>
   );
 }
