bbdeed3d8aaf67044b13549c0467359d269d0999
benalak11 <mm8584856@gmail..com>
2025-09-07 23:38:49 +0100

    task 2

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,6 +4,18 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import ProfilePage from './ProfilePage';
+import UserContext from './src/components/UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
 
 
 
