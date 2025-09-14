e570867c9af06d19ec4157c58da03f0909cb6f55
benalak11 <mm8584856@gmail..com>
2025-09-14 23:37:45 +0100

    task 0

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
 // src/components/RecipeList.jsx
 import React from 'react';
 import { useRecipeStore } from '../store/recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
@@ -10,11 +11,15 @@ const RecipeList = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {filteredRecipes.map(recipe => (
-        <div key={recipe.id} className="border p-4 rounded shadow">
+        <Link
+          to={`/recipe/${recipe.id}`}
+          key={recipe.id}
+          className="border p-4 rounded shadow block hover:shadow-lg transition"
+        >
           <h3 className="font-bold text-lg">{recipe.title}</h3>
           <p>Preparation Time: {recipe.prepTime} mins</p>
           <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
