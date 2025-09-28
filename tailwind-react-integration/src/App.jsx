0a1ee015481822123080b3a68400e6771eda86a6
benalak11 <mm8584856@gmail..com>
2025-09-28 22:25:40 +0100

    task 0

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -41,5 +41,14 @@ function App() {
     </div>
   )
 }
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
 
 export default App
