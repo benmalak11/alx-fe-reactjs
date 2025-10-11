7f1a87ac8560d71d5c53a0db15fa76c95897223e
benalak11 <mm8584856@gmail..com>
2025-10-11 23:18:54 +0100

    task 0

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -32,4 +32,15 @@ function App() {
   )
 }
 
-export default App
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>My Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
